b8f574db803562865225fddbdba594ac619dfe7d
Jonathan Guzmán <jonguz5@hotmail.com>
2022-04-25 22:51:10 -0600

    SQLiteConnection

--- src/main/java/net/xravn/dboilerplate/database/connection/GenericDBConnection.java
+++ src/main/java/net/xravn/dboilerplate/database/connection/GenericDBConnection.java
@@ -4,16 +4,66 @@ import java.sql.Connection;
 
 public abstract class GenericDBConnection implements DBConnection {
 
-    protected Connection generateConnection(String driver, String connectionString) throws Exception {
-        try {
-            Class.forName(driver);
-            return java.sql.DriverManager.getConnection(connectionString);
-        }
-        // check for driver not found
-        catch (ClassNotFoundException e) {
-            System.err.println("Could not find the driver: " + driver);
-        }
-        return null;
+    protected GenericDBConnection(String driver) throws ClassNotFoundException {
+        Class.forName(driver);
     }
+
+    protected GenericDBConnection(String driver, String connectionString) throws ClassNotFoundException {
+        this(driver);
+        this.connectionString = connectionString;
+    }
+
+    protected String getConnectionString() {
+        return connectionString;
+    }
+
+    protected String setConnectionString(String connectionString) {
+        return this.connectionString = connectionString;
+    }
+
+    @Override
+    public Connection getConnection() {
+        synchronized (lock) {
+            if (!isConnected()) {
+                try {
+                    connection = java.sql.DriverManager.getConnection(connectionString);
+                } catch (Exception e) {
+                    System.err.println("Could not connect to database: " + e.getMessage());
+                    return null;
+                }
+            }
+            return connection;
+        }
+    }
+
+    @Override
+    public boolean isConnected() {
+        synchronized (lock) {
+            try {
+                return connection == null ? false : !connection.isClosed();
+            } catch (Exception e) {
+                return false;
+            }
+        }
+    }
+
+    @Override
+    public void closeConnection() {
+        synchronized (lock) {
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (Exception ex) {
+                    System.err.printf("Could not close SQLite connection: %s\n",
+                            ex.getLocalizedMessage());
+                }
+                connection = null;
+            }
+        }
+    }
+
+    private Connection connection = null;
+    private String connectionString = null;
+    private final Object lock = new Object();
 
 }
--- src/main/java/net/xravn/dboilerplate/sqlite/connection/SQLiteConnection.java
+++ src/main/java/net/xravn/dboilerplate/sqlite/connection/SQLiteConnection.java
@@ -1,52 +1,26 @@
 package net.xravn.dboilerplate.sqlite.connection;
 
-import java.sql.Connection;
-
 import net.xravn.dboilerplate.database.connection.GenericDBConnection;
 
 public class SQLiteConnection extends GenericDBConnection {
 
+    public SQLiteConnection() throws ClassNotFoundException {
+        super(DRIVER);
+    }
+
+    private String generateConnectionString() {
+        return String.format(CONNECTION_STRING, database);
+    }
+
     @Override
     public void setCredentials(String database, String user, String password) {
         this.database = database;
-    }
-
-    @Override
-    public Connection getConnection() throws Exception {
-        synchronized (lock) {
-            if (connection.isClosed()) {
-                connection = generateConnection("org.sqlite.JDBC", "jdbc:sqlite:" + database);
-            }
-            return connection;
-        }
-    }
-
-    @Override
-    public boolean isConnected() {
-        return connection == null;
-    }
-
-    @Override
-    public void closeConnection() {
-        synchronized (lock) {
-            if (connection != null) {
-                try {
-                    connection.close();
-                } catch (Exception ex) {
-                    System.err.printf("No se pudo cerrar la conexión con la base de datos: %s\n",
-                            ex.getLocalizedMessage());
-                    // close the connection anyway
-                    connection = null;
-                }
-            }
-        }
+        super.setConnectionString(generateConnectionString());
     }
 
     private static final String DRIVER = "org.sqlite.JDBC";
     private static final String CONNECTION_STRING = "jdbc:sqlite:%s";
     // by default a memory database is created
     private String database = ":memory:";
-    private Connection connection = null;
-    private final Object lock = new Object();
 
 }
