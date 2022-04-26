420bff8dffe35e8726fc884655f878a7610dbbce
Jonathan Guzmán <jonguz5@hotmail.com>
2022-04-25 23:49:25 -0600

    add cli

--- src/main/java/net/xravn/dboilerplate/DBoilerplate.java
+++
@@ -1,9 +0,0 @@
-package net.xravn.dboilerplate;
-
-
-public class DBoilerplate {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
---
+++ src/main/java/net/xravn/dboilerplate/cli/DBoilerplateCLI.java
@@ -0,0 +1,22 @@
+package net.xravn.dboilerplate.cli;
+
+import java.util.concurrent.Callable;
+
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+
+@Command(name = "dboilerplate", mixinStandardHelpOptions = true, version = "0.0.1", //
+        description = "DBoilerplate is a tool to reduce (D)atabase (B)oilerplate code.")
+public class DBoilerplateCLI implements Callable<Void> {
+
+    public static void main(String[] args) {
+        int exitCode = new CommandLine(new DBoilerplateCLI()).execute(args);
+        System.exit(exitCode);
+    }
+
+    @Override
+    public Void call() {
+        System.out.println("DBoilerplate is a tool to reduce (D)atabase (B)oilerplate code.");
+        return null;
+    }
+}
